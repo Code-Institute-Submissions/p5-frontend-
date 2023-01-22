c68bc6a72beea228667d9ed7eb1cb2f307804986
FRNBDN <francesco.badini94@gmail.com>
2023-01-22 14:31:45 +0000

    add List.js, import to detailpage, link to api

---
+++ src/pages/todolist/List.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Col, ToggleButton, Row} from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import Avatar from '../../components/Avatar';
+
+const List = (props) => {
+    const {
+        id,
+        completed,
+        title,
+        description,
+        is_owner,
+        items_count,
+        owner,
+        profile_id,
+        profile_image
+
+    } = props
+  return (
+    <Card>
+        <Card.Header><span className=''>{title}</span><span className="text-muted">, created by: {owner}<Avatar src={profile_image}/></span></Card.Header>
+        <Card.Body>
+            <Card.Text>
+                {description}
+            </Card.Text>
+            <Card.Body>
+                {completed? (
+                     <p>List is completed</p>
+                ): (
+                    <>
+                        <Row>
+                            <Col>
+                                Item1
+                            </Col>
+                            <Col>
+                                Item2
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                Item3
+                            </Col>
+                            <Col>
+                                Item4
+                            </Col>
+                        </Row>
+                    </>
+                )}
+            </Card.Body>
+            {is_owner?(
+                <Card.Footer className="text-muted">
+                    <span>Items:{items_count} </span>
+                    <ToggleButton
+                        className="mb-2"
+                        id="toggle-check"
+                        type="checkbox"
+                        variant="outline-primary"
+                        checked={completed}
+                        value="1"
+                        onChange={() => {}}
+                        >
+                        Completed
+                      </ToggleButton>
+                </Card.Footer>
+
+            ):(
+                <Card.Footer className="text-muted">
+                <span>Items:{items_count} </span>
+                </Card.Footer> 
+            )}
+            
+        </Card.Body>
+    </Card>
+  )
+}
+
+export default List
--- src/pages/todolist/ListDetailPage.js
+++ src/pages/todolist/ListDetailPage.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+import { axiosReq } from '../../api/axiosDefaults';
+import List from './List';
 
 export default function ListDetailPage() {
-    
+    const {id} = useParams();
+    const [list, setList] = useState({results: []})
+
+    useEffect(()=>{
+        const handleMount = async () =>{
+            try {
+                const [{data: list}] = await Promise.all([
+                    axiosReq.get(`/lists/${id}`)
+                ])
+                setList({results:[list]})
+                console.log(list)
+            } catch (error) {
+                console.log(error);  
+            }
+        }
+        handleMount();
+    },[id])
 
   return (
     <Row>
         <Col>
          <p>My Other Lists</p>
-         <p>List detail</p>
+         <List {...list.results[0]} setLists={setList}/>
          <Container>Comments</Container> 
         </Col>
         <Col>
